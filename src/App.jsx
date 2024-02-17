55dd792fa0e25b72f80418e485a010795385558e
Xaphan64 <alex_daniel64@yahoo.com>
2024-02-17 10:11:57 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+// ASSETS
+
+// STYLES
+
+// LIBRARIES
+
+// MISC
+
+// COMPONENTS
+
+// CONFIGURATION
+const App = () => {
+  // PROPERTIES
+
+  // API REQUESTS
+
+  // LIBRARY CONSTANTS
+
+  // STATE CONSTANTS
+
+  // LIFE CYCLE
+
+  // EVENT HANDLERS
+  return (
+    <div>
+      <h1>MY PORTFOLIO</h1>
+    </div>
+  );
+};
+
+export default App;
