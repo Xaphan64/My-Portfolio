43f8d0959d99503e384735e7cc35d29957b27b88
Xaphan64 <alex_daniel64@yahoo.com>
2024-02-21 21:06:50 +0200

    added projects and project styling

---
+++ src/elements/components/Project/Project.jsx
@@ -0,0 +1,34 @@
+// ASSETS
+
+// STYLES
+
+// LIBRARIES
+
+// MISC
+
+// COMPONENTS
+
+// CONFIGURATION
+const Project = ({ image, name }) => {
+  // PROPERTIES
+
+  // API REQUESTS
+
+  // LIBRARY CONSTANTS
+
+  // STATE CONSTANTS
+
+  // LIFE CYCLE
+
+  // EVENT HANDLERS
+  return (
+    <div className="project-container">
+      <div style={{ backgroundImage: `url(${image})` }} className="project-image" />
+      <h1> {name} </h1>
+    </div>
+  );
+};
+
+export default Project;
+
+//0722 397 297 - laurentiu sun din partea lui marian de la royal pt revizia de gaze
--- src/elements/pages/Projects/Projects.jsx
+++ src/elements/pages/Projects/Projects.jsx
@@ -1,12 +1,17 @@
 // ASSETS
+import blog from "../../assets/blog_placeholder.webp";
+import ecommerce from "../../assets/ecommerce_placeholder.jpg";
+import pm from "../../assets/pm_placeholder.avif";
 
 // STYLES
+import "./Projects.scss";
 
 // LIBRARIES
 
 // MISC
 
 // COMPONENTS
+import Project from "../../components/Project/Project";
 
 // CONFIGURATION
 const Projects = () => {
@@ -23,7 +28,13 @@ const Projects = () => {
   // EVENT HANDLERS
   return (
     <div className="projects-container">
-      <h1>Projects component</h1>
+      <h1>My Projects</h1>
+
+      <div className="projects-list">
+        <Project name="hardcoded blog project" image={blog} />
+        <Project name="hardcoded pm project" image={pm} />
+        <Project name="hardcoded ecommerce project" image={ecommerce} />
+      </div>
     </div>
   );
 };
